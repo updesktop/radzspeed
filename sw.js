da6a77a29d5998849523c03d833047cb8db10372
UPDESKTOP Digital <updesktop@gmail.com>
2026-07-19 09:30:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,36 @@
-const CACHE_NAME = 'negros-tires-v1';
-const ASSETS_TO_CACHE = ['/'];
+const CACHE_NAME = 'crud-pwa-local-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icon-192.png'
+];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE)));
-    self.skipWaiting();
+// Install and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(err => console.log('Cache error:', err))
+  );
 });
 
-self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(
+// Remove old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
         keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    )));
-    self.clients.claim();
+      )
+    )
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(cached => {
-            return cached || fetch(e.request).then(res => {
-                const resClone = res.clone();
-                caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
-                return res;
-            });
-        })
-    );
+// Serve from cache or network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => cachedResponse || fetch(event.request))
+  );
 });
